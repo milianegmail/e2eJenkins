445e1e41eaddec1bae06d4852abb65e3ce218e12
milianegmail <miliane.madeira@gmail.com>
2023-01-26 14:45:15 -0300

    e2e

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+//import login from '../integration/login.spec'
+//const perfil = require('..//fixtures/perfil.json')
+let dadosLogin
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -14,7 +17,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil
+        })
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha)
+        })
+        var qtd = 4
+        cy.visit('produtos/page/2/')
+        cy.addProdutos('Atomic Endurance Running Tee (Crew-Neck)', 'L', 'Red', 1)
+        cy.visit('produtos/page/2/')
+        cy.addProdutos('Atomic Endurance Running Tee (V-neck)', 'XL', 'Green', 1)
+        cy.visit('produtos/page/2/')
+        cy.addProdutos('Autumn Pullie', 'XS', 'Green', 1)
+        cy.visit('produtos/page/2/')
+        cy.addProdutos('Augusta Pullover Jacket', 'S', 'Blue', 1)
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', qtd)
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.get('#place_order').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('.woocommerce-order-overview__order').should('contain', "Número do pedido:")
+
+
     });
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,13 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add("addProdutos", (produto, tamanho, cor, qtd) => {
+    //cy.get('.post-2559 > .product-block > .caption > .meta > .infor > .name > a')
+    cy.get('[class="product-block grid"]')
+        .contains(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(qtd)
+    cy.get('.single_add_to_cart_button').click()
+});
+
